272e34da564b6b5a02de69aa76a205291e25348e
Diogo Vitor <dihleme@hotmail.com>
2023-08-23 14:45:34 -0300

    first commit

---
+++ app/conexaoBD/conecta.php
@@ -0,0 +1,10 @@
+<?php
+
+define("SERVERNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DBNAME", "desafio_4etapa");
+
+$conexao = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+
+?>
---
+++ app/conexaoBD/conexao.php
@@ -0,0 +1,10 @@
+<?php
+
+define("NOMESERVIDOR", "localhost");
+define("USUARIO", "root");
+define("SENHA", "");
+define("NOMEbd", "desafio_4etapa");
+
+$conexao = mysqli_connect(NOMESERVIDOR, USUARIO, SENHA, NOMEbd);
+
+?>
